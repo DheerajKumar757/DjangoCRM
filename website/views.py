361df60199c533024a58f07fd447fb0efd1036cd
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2024-09-14 20:49:36 +0530

    Added the navbar using css Added some wepages as well in the app
    Signed-off-by: Dheeraj Kumar <dheerajkumar12757@gmail.com>

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    
+    return render(request, 'home.html', {})
+    #template = loader.get_template('home.html')
+    #return HttpResponse(template.render({}, request))
