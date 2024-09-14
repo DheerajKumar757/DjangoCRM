e68900a2ea0ff419c7befd988eb473f3b85f7c4f
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2024-09-14 22:50:53 +0530

    Implementation of logout feature into CRM
    Signed-off-by: Dheeraj Kumar <dheerajkumar12757@gmail.com>

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # use this for seperate login page.
+    # In this project we are using home page as login page.
+    #path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
